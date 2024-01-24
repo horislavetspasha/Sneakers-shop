f7efa10c3a25701c294ed66ce64c8744ab359aa0
Horislavets Pavlo <horislavetspasha@gmail.com>
2024-01-24 21:14:32 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from "@/components/Header.vue";
+import CardList from "@/components/CardList.vue";
+</script>
+<template>
+  <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14">
+    <Header />
+
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">All Sneakers</h2>
+      <CardList />
+    </div>
+  </div>
+</template>
+<style scoped>
+</style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Card from "@/components/Card.vue";
+</script>
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card title="Mens sneakers Nike Blazer" :price="5000" imageUrl="/sneakers/sneakers-1.jpg" :is-favorite="false"/>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
